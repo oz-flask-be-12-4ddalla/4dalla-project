d0232b5ba8819500763ad6e476a30dbdbd362a3c
hoochucat <ohnanabanha@gmail.com>
2025-07-21 15:31:25 +0900

    🐛 기능 수정, 버그 수정
    선택지 기능 코드 수정

---
+++ app/routes/images.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify, request
+from config import db
+from ..models import Image
+
+images_bp = Blueprint("images",__name__)
+
+@images_bp.route("/image", methods=["POST"])
+def create_image():
+    data = request.get_json()
+
+    # 유효성 검사
+    if not data.get("url") or not data.get("type"):
+        return jsonify({"message": "url 또는 type이 누락되었습니다."}), 400
+
+    # 이미지 객체 생성
+    image = Image(
+        url=data["url"],
+        type=data["type"]
+    )
+
+    # DB에 저장
+    try:
+        db.session.add(image)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"message": "DB 저장 중 오류가 발생했습니다.", "error": str(e)}), 500
+
+    return jsonify({"message" :f"ID: {image.id} Image Success Create" }), 201
+
+
+@images_bp.route("/image/main", methods=["GET"])
+def get_main_image():
+    image = Image.query.filter_by(type="main").first()
+
+    if not image : 
+        return jsonify({"message" : "메인 이미지가 없습니다."}), 404
+    return jsonify({"image":image.url})
