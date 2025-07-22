25b09d0c845cb06598e8f054c281af880e58ea7b
lsh9511 <iamshopsh@naver.com>
2025-07-22 13:05:51 +0900

    🚑️ 최종본 rum.py 호스트, 포트 기본값 변경

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(debug=True, host='0.0.0.0', port=8000)
+    
